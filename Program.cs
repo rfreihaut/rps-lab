3bff87472d5cb97c9bd0b7af8feef830620406c4
Robert Freihaut <rfreihaut@aol.com>
2019-08-26 17:02:55 -0400

    rps lab

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RPSBreakout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RPSEnum.Rock);
+            Rocky r = new Rocky("Rock");
+            Human h = new Human("Dywane Johnson");
+
+            
+
+            Random rando = new Random();
+            Random randy = new Random();
+
+            RandomPlayer rp = new RandomPlayer("Bob", randy);
+            for (int i = 0; i < 100; i++)
+            {
+                 
+                Console.WriteLine(rp.GetRPS());
+            }
+        }
+    }
+}
